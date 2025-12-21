3efdf97a89e6fc043005c1bcba030aabec780f06
VanshGolakiya-2910 <vanshgolakiya11@gmail.com>
2025-12-21 09:47:28 +0530

    added : frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,13 @@ const app = express();
 // ───────────────────────────────
 // Middleware
 // ───────────────────────────────
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    credentials: true,
+  })
+);
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
